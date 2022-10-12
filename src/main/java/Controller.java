f10932ce68fd9497f3f4a296c3c666b201fbb18e
Felicia Kenney <fkenney@bravocg.com>
2022-10-12 17:00:27 -0400

    gets commands from user AB#133

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,28 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Controller {
-    View view = new View();
-    private static final String LEVEL = "1";
-
-    public void gameSetUp(){
-      String name = null;
-      System.out.println(view.getGameBanner());
-
-      System.out.println(view.getHelpCommands());
-      try{
-        System.out.println(view.getNamePrompt());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        name = reader.readLine();
-        name.trim();
-        System.out.printf(view.getStoryIntro(), name);
-
-        System.out.println(view.getInstructions());
-        System.out.printf(view.getStatusBanner(), "Test Current Location", "[Test Item]");
-      }catch (IOException e){
-        throw new RuntimeException(e);
+  View view = new View();
+  private static final String LEVEL = "1";
+  String name;
+  public boolean gameSetUp(){
+    String input = null;
+    boolean start = false;
+    System.out.println(view.getGameBanner());
+    System.out.println(view.getHelpCommands());
+    System.out.println(view.getInstructions());
+    try{
+      System.out.println("Would you like to start the game? enter ('yes' or 'no') ");
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      input = reader.readLine();
+      input.toLowerCase().trim();
+      if (input.equals("yes")){
+        start = true;
+        getPlayerName();
       }
-
+    }catch (IOException e){
+      throw new RuntimeException(e);
     }
+    return start;
+  }
+  public void getPlayerName(){
+    try{
+      System.out.println(view.getNamePrompt());
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      name = reader.readLine();
+      name.trim();
+      System.out.printf(view.getStoryIntro(), name);
+      System.out.printf(view.getStatusBanner(), "Test Current Location", "[Test Item]");
+    }catch (IOException e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  public boolean getCommand(){
+    String[] command;
+    String input = null;
+    try{
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      input = reader.readLine();
+      input = input.toLowerCase().trim();
+      command = input.split("\\s+");
+      System.out.println(Arrays.toString(command));
+    }catch (IOException e){
+      throw new RuntimeException(e);
+    }
+    return !command[0].equals("quit");
+  }
+
+
 }
